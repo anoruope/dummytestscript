f264e2a38e648ac4fdc86359a5d0d362dd451456
anoruope <Maria@45>
2020-02-18 10:26:57 +0100

    updated the testscript

--- tester.py
+++ tester.py
@@ -5,6 +5,7 @@ from random import randint
 import json
 import os
 from os import path
+import time
 
 val1 = "Harz"
 val2 = "Haerter"
@@ -14,20 +15,39 @@ val5 = "fuellstoff3"
 arrd = [val1,val2,val3,val4,val5]
 mqtturl = os.getenv("MQTT_URL")
 mqttport = int(os.getenv("MQTT_PORT"))
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag = True
+        print("connected Returned code =",rc)
+    else:
+        client.connected_flag = False
+        print("Bad connection Returned code=",rc)
+
 def testlogic():
-    seed(1)
-    print("Process Started....")
-    while True:
-        try:
-            for v in arrd:
-                for _ in range(1):
-                    value = randint(0,10)
-                    client = mqtt.Client()
-                    client.connect(mqtturl,mqttport,60)
-                    client.publish("sensor", json.dumps({"sensor": v+"Box", "value":float(value)}))
-                    #print( v+"Box" + " " + str(value))
-        except:
-            print("doings the rounds")
+    try:
+        mqtt.Client.connected_flag = False
+        seed(1)
+        print("Process Started....")
+        client = mqtt.Client("Jaded")
+        client.on_connect = on_connect
+        client.loop_start()
+        print("Connecting to broker ",mqtturl)  
+        client.connect(mqtturl,mqttport,300)
+        while not client.connected_flag:
+            print("In wait loop")
+            time.sleep(4)
+        while True:
+            try:
+                for v in arrd:
+                    for _ in range(1):
+                        value = randint(0,10)
+                        client.publish("sensor", json.dumps({"sensor": v+"Box", "value":float(value)}))
+                        print(v+"Box " + str(value))
+            except:
+                print("Exception in publish")
+    except:
+        print("Exception in connection")
 
 
 testlogic()
