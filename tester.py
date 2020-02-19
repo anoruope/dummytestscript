6930932504244f1609fcc79bee589891565bcf21
anoruope <Maria@45>
2020-02-19 13:42:03 +0100

    commit

--- tester.py
+++ tester.py
@@ -43,11 +43,12 @@ def testlogic():
                 for v in arrd:
                     for _ in range(1):
                         value = randint(0,10)
-                        client.publish("sensor", json.dumps({"sensor": v+"Box", "value":float(value)}))M
+                        client.publish("sensor", json.dumps({"sensor": v+"Box", "value":float(value)}))
             except:
                 print("Exception in publish")
     except:
         print("Exception in connection")
+        
 
 
 testlogic()
